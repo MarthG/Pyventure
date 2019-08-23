d6562dcfffd721599c3788922e7c01a9d9b223a9
MarthG <cgcarlos444@gmail.com>
2019-08-23 14:45:59 +0200

    work on __main__, status bits WIP

--- __main__.py
+++ __main__.py
@@ -1,10 +1,34 @@
 from sys import argv
 
 def main(argv=None):
-    if len(argv) <= 1:
-        raise Exception("Null arguments")
-        return -1
-    print("argc: {0}\nargv: {1}".format(len(argv)-1, argv))
+    arg = argv[1:]
+    help_arg = ["-h", "-?", "--help"]
+    vers_arg = ["-v", "-ver", "--version"]
+
+    # Status bits
+    # normal_execution,1 arg_called,2 help_arg,4 vers_arg,8
+    status = 0
+    if len (arg) >= 1:
+        status |= 2
+        for i in arg:
+            if i in help_arg:
+                status |= 4
+            elif i in vers_arg:
+                status |= 8
+            else:
+                print("Bad arguments. Try {0} for help".format(help_arg[1]))
+                return -1
+    else:
+        status |= 1
+
+    checkStatus(status)
+
+    return 0
+
+def checkStatus(currentStatus=None):
+    if (currentStatus is None):
+        pass
+
     return 0
 
 if __name__ == '__main__':
