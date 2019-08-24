379df1a4c1dae4e8c7dcc568e29a8e2f118473d6
MarthG <cgcarlos444@gmail.com>
2019-08-24 15:37:44 +0200

    even more help()

--- pyventure.py
+++ pyventure.py
@@ -3,20 +3,22 @@ from sys import argv
 verbosity = 0
 version = "0.01"
 
-def help(cmds=None):
+def help(cmds=None, command_descriptions=None):
     global verbosity
 
-    if (cmds is None):
+    if (cmds is None) or (command_descriptions is None):
         raise Exception("[EE] Exception: Can't display help message, something went wrong.")
 
     if verbosity > 0:
         print("[II] We are verbose")
 
     print("[II] Stub help()")
-    
-    for cmd in cmds:
-        print("")
+
+    for command in cmds:
+        #print(command)
+        print("[II] {0}: {1}".format(command, command_descriptions[str(command)]))
     pass
+
 
 def version():
     global verbosity
@@ -31,6 +33,7 @@ def version():
 )
     pass
 
+
 def run():
     global verbosity
 
@@ -40,13 +43,18 @@ def run():
 
     pass
 
+
 def main(argv=None):
     arg = argv[1:]
-    cmds = [help_arg, vers_arg, verbose]
-    descriptions = {help_arg: "", vers_arg: "", verbose: ""}
+
     help_arg = ["-h", "-?", "--help"]
     vers_arg = ["-v", "-ver", "--version"]
     verbose = ["-V", "--debug", "--verbose"]
+
+    # I know this is \extremely/ inelegant, but it works.
+    descriptions = {str(help_arg): "help", str(vers_arg): "version", str(verbose): "debug"}
+
+    cmds = [help_arg, vers_arg, verbose]
 
     global verbosity
 
@@ -72,12 +80,13 @@ def main(argv=None):
     else:
         status |= 1
 
-    print(verbosity)
-    checkStatus(status, help(cmds), version, run)
+    # print(verbosity)
+    checkStatus(status, help, version, run, cmds, descriptions)
 
     return 0
 
-def checkStatus(currentStatus=None, helpFunction=None, versionFunction=None, normalRuntime=None):
+
+def checkStatus(currentStatus=None, helpFunction=None, versionFunction=None, normalRuntime=None, cmds=None, descriptions=None):
     # # # # checkStatus(status)
     # Flag: [PRIORITY]
     # Verbosity:        +++
@@ -91,21 +100,30 @@ def checkStatus(currentStatus=None, helpFunction=None, versionFunction=None, nor
 
     global verbosity
 
-    if (helpFunction is None) or (versionFunction is None) or (normalRuntime is None):
-        raise Exception("[EE] Exception: Bad function pass")
+    if (helpFunction is None):
+        raise Exception("[EE] Exception: Bad help()")
+    if (versionFunction is None):
+        raise Exception("[EE] Exception: Bad version()")
+    if (normalRuntime is None):
+        raise Exception("[EE] Exception: Bad runtime")
+
+
 
     if (a != 0) and (not a is None):
         if (a & 2):
             if (a & 4):
-                helpFunction()
+                helpFunction(cmds, descriptions)
+                return 0
             elif (a & 8):
                 versionFunction()
+                return 0
             else:
                 raise Exception("[EE] Exception: Invalid state. Exiting")
         elif (a & 1):
             normalRuntime()
 
     return 0
+
 
 if __name__ == '__main__':
     main(argv)
