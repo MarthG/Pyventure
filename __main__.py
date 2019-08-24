13d2996eab3fcf9fa82b86621b42001e4a0652ee
MarthG <cgcarlos444@gmail.com>
2019-08-24 13:33:18 +0200

    more __main__

--- __main__.py
+++ __main__.py
@@ -1,13 +1,53 @@
 from sys import argv
 
+verbosity = 0
+version = "0.01"
+
+def help():
+    global verbosity
+
+    if verbosity > 0:
+        print("[II] We are verbose")
+        print("[II] Stub help()")
+
+    pass
+
+def version():
+    global verbosity
+    if verbosity > 0:
+        print("[II] We are verbose")
+    print(
+'''
+[II]
+[II] Version v{0}
+[II]
+'''.format(version)
+)
+    pass
+
+def run():
+    global verbosity
+
+    if verbosity > 0:
+        print("[II] We are verbose")
+    print("[II] Stub run()")
+
+    pass
+
 def main(argv=None):
     arg = argv[1:]
     help_arg = ["-h", "-?", "--help"]
     vers_arg = ["-v", "-ver", "--version"]
+    verbose = ["-V", "--debug", "--verbose"]
+
+    global verbosity
 
     # Status bits
-    # normal_execution,1 arg_called,2 help_arg,4 vers_arg,8
+    # normal_execution,1 arg_called,2 help_arg,4 vers_arg,8 verbose,16
     status = 0
+
+    # print(arg)
+
     if len (arg) >= 1:
         status |= 2
         for i in arg:
@@ -15,19 +55,47 @@ def main(argv=None):
                 status |= 4
             elif i in vers_arg:
                 status |= 8
+            elif i in verbose:
+                status |= 16
+                verbosity = 1
             else:
-                print("Bad arguments. Try {0} for help".format(help_arg[1]))
+                print("[EE] Bad arguments. Try {0} for help".format(help_arg[1]))
                 return -1
     else:
         status |= 1
 
-    checkStatus(status)
+    print(verbosity)
+    checkStatus(status, help, version, run)
 
     return 0
 
-def checkStatus(currentStatus=None):
-    if (currentStatus is None):
-        pass
+def checkStatus(currentStatus=None, helpFunction=None, versionFunction=None, normalRuntime=None):
+    # # # # checkStatus(status)
+    # Flag: [PRIORITY]
+    # Verbosity:        +++
+    # Arguments called: +++
+    # Help arg:     ++
+    # Version arg:  ++
+    # Normal:       +
+    #
+    # Usage: checkStatus( State variable, help f(), version f(), run f())
+    a = currentStatus
+
+    global verbosity
+
+    if (helpFunction is None) or (versionFunction is None) or (normalRuntime is None):
+        raise Exception("[EE] Exception: Bad function pass")
+
+    if (a != 0) and (not a is None):
+        if (a & 2):
+            if (a & 4):
+                helpFunction()
+            elif (a & 8):
+                versionFunction()
+            else:
+                raise Exception("[EE] Exception: Invalid state. Exiting")
+        elif (a & 1):
+            normalRuntime()
 
     return 0
 
